7ff1b433c5ed52a07dd4aa146d47419d8a3575a5
Jakub Dorda <jak_dor@wp.pl>
2017-08-23 20:45:51 +0200

    add layout for info tab

--- ssc/app/src/main/java/com/jakdor/sscapp/Info/InfoFragment.java
+++ ssc/app/src/main/java/com/jakdor/sscapp/Info/InfoFragment.java
@@ -9,11 +9,15 @@ import android.view.ViewGroup;
 import com.jakdor.sscapp.BaseFragment;
 import com.jakdor.sscapp.R;
 
+import butterknife.ButterKnife;
+
 public class InfoFragment extends BaseFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.info_fragment, container, false);
+        View view = inflater.inflate(R.layout.info_fragment, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 }
